d61981ff45620c6e339c6ef2bf4fdf582032ff32
Abhay-2k01 <55129184+Thakur-Abhay@users.noreply.github.com>
2024-12-15 19:08:36 +0530

    moved functions into main.py to resolve import conflicts

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import openai
 from openai import OpenAI, AsyncOpenAI
 from PIL import Image
 import os
-from .context_enricher import GenericEnrichmentEngine
+from pdf2image import convert_from_path
+import cv2
+import numpy as np
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
@@ -20,11 +22,56 @@ def API_call(prompt):
     answer = response.choices[0].text
     return answer
 
-
-preprocesser_obj = GenericEnrichmentEngine()
 input_pdf = "C:/Users/abhay/OneDrive/Desktop/AT/Document-QA/gate_scorecard.pdf"
-preprocesser_obj.pdf_to_image(input_pdf)
-preprocesser_obj.enhance_image_quality("pdf_image.jpg")
+
+
+def pdf_to_image(pdf_path):
+    """
+    Converts a PDF file to an image format.
+
+    Args:
+        pdf_path (str): The path to the PDF file to be converted.
+
+    Returns:
+        An image or a list of images representing the pages of the PDF.
+    """
+    images = convert_from_path(pdf_path)
+    images[0].save("pdf_image.jpg", "JPEG")
+
+
+def enhance_image_quality(image_path):
+    """
+    Enhances the quality of an input image.
+
+    Args:
+        image_path (str): Path to the input image.
+
+    Returns:
+        numpy.ndarray: Enhanced image.
+    """
+    # Read the input image
+    image = cv2.imread(image_path)
+
+    # Apply Gaussian blur to reduce noise
+    blurred_image = cv2.GaussianBlur(image, (5, 5), 0)
+
+    # Apply histogram equalization to improve contrast
+    lab_image = cv2.cvtColor(blurred_image, cv2.COLOR_BGR2LAB)
+    l, a, b = cv2.split(lab_image)
+    clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8, 8))
+    cl = clahe.apply(l)
+    limg = cv2.merge((cl, a, b))
+    enhanced_image = cv2.cvtColor(limg, cv2.COLOR_LAB2BGR)
+
+    # Apply sharpening to improve details
+    kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
+    sharpened_image = cv2.filter2D(enhanced_image, -1, kernel)
+    cv2.imwrite("enhanced_image.jpg", sharpened_image)
+
+
+pdf_to_image(pdf_path=input_pdf)
+enhance_image_quality("pdf_image.jpg")
+
 image = Image.open("enhanced_image.jpg")
 
 user_query = "What is the name mentioned in the gate scorecard?"
