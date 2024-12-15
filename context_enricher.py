2f40f6fe3d79b4a2c950cdf358ba4f4a11e2ba97
Abhay-2k01 <55129184+Thakur-Abhay@users.noreply.github.com>
2024-12-15 17:08:10 +0530

    initial commit, initialized helper functions

---
+++ context_enricher.py
@@ -0,0 +1,12 @@
+class GenericEnrichmentEngine:
+    def __init__(self):
+        pass
+
+    async def pdf_to_image(self, pdf_path):
+        pass
+
+    async def image_enhancer(self, image_path):
+        pass
+
+    async def pdf_parser(self, pdf_path):
+        pass
