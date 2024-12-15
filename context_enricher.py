bbb2e9ef4ccf9be9abd1e7c380df98b82e5917cc
Abhay-2k01 <55129184+Thakur-Abhay@users.noreply.github.com>
2024-12-15 17:37:51 +0530

    method to convert image of 1st page and save it from a pdf

--- context_enricher.py
+++ context_enricher.py
@@ -1,12 +1,25 @@
+from pdf2image import convert_from_path
+import cv2
+import numpy as np
+
+
 class GenericEnrichmentEngine:
     def __init__(self):
         pass
 
     async def pdf_to_image(self, pdf_path):
-        pass
+        """
+        Converts a PDF file to an image format.
 
-    async def image_enhancer(self, image_path):
-        pass
+        Args:
+            pdf_path (str): The path to the PDF file to be converted.
+
+        Returns:
+            An image or a list of images representing the pages of the PDF.
+        """
+        images = convert_from_path(pdf_path)
+        images[0].save("pdf_image.jpg", "JPEG")
+
 
     async def pdf_parser(self, pdf_path):
         pass
